fcc48b4329f655b5af670ebc4c007d5e484d754d
fili5863​ <fili5863@stud.kea.dk>
2023-03-20 19:43:14 +0100

    oneball2

--- Design for developers/onemotion_exercise/multiballs.js
+++ Design for developers/onemotion_exercise/multiballs.js
@@ -1,1 +1,15 @@
 "use strict";
+
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
+
+animate(
+  ".ball",
+  { y: -400 },
+  {
+    delay: stagger(0.2),
+    duration: 1,
+    easing: "ease-in-out",
+    repeat: Infinity,
+    direction: "alternate",
+  }
+);
