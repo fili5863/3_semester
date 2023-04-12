5e94f926f4e5528732665b9e97c1c33e719f4a33
fili5863​ <fili5863@stud.kea.dk>
2023-04-12 11:12:20 +0200

    øvelser done

--- Theme_14c_2_React/index.js
+++ Theme_14c_2_React/index.js
@@ -1,4 +1,4 @@
-const ages = [1, 3, 4, 6, 7];
+/* const ages = [1, 3, 4, 6, 7]; */
 
 // ages.forEach.call(items => {}); //we do stuff
 
@@ -11,7 +11,7 @@ const ages = [1, 3, 4, 6, 7];
 
 // objects for in;
 
-const name = "Jonas";
+/* const name = "Jonas";
 
 name[0];
 
@@ -33,4 +33,138 @@ const ar2 = [...ar11, ...ar12];
 const car = { name: "peugeot", model: "5008" };
 const car2 = { ...car, wheels: 4, model: 308 };
 
-console.log(car2);
+console.log(car2); */
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 1 */
+
+const age = 42;
+const email = "jofh@kea.dk";
+
+/* if (age % 2 === 0) {
+  isEven = true;
+} else {
+  isEven = false;
+} */
+
+/* let isEven = age % 2 === 0 ? true : false; */
+
+/* let role = "visitor";
+if (email === "jofh@kea.dk") {
+  if (age > 40) {
+    role = "admin";
+  }
+} */
+
+/* let role = email === "jofh@kea.dk" && age > 40 ? "admin" : "visitor"; */
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 2 */
+
+const setTitle = title => (document.title = title);
+
+const isEven = number => (number % 2 === 0 ? true : false);
+
+const getRandBetween1and10 = () => Math.floor(Math.random() * 10) + 1;
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 3 */
+
+const movies = [
+  {
+    name: "Hard Boiled",
+    genre: "Hong Kong Action",
+  },
+  {
+    name: "LOTR",
+    genre: "Fantasy",
+  },
+];
+
+movies.forEach(movie => console.log(`The movie ${movie.name} is a typical ${movie.genre} movie`));
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 4 */
+
+const searchengines = [
+  { target: "https://google.com", name: "Google" },
+  { target: "https://bing.com", name: "Bing" },
+];
+let links = searchengines.map(link => `<a href="${link.target}">${link.name}</a>`);
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 5 */
+
+const names = ["Jonas", "Dannie", "Peter", "Klaus"];
+
+const myBadCopy = names;
+
+myBadCopy[1] = "Jachob";
+
+console.log(names);
+console.log(myBadCopy);
+
+const myGoodCopy = [...names];
+
+names[1] = "Jachob";
+
+console.log(names);
+console.log(myGoodCopy);
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 6 */
+
+const me = {
+  name: "Jonas",
+  age: 43,
+  kids: 3,
+};
+
+const meCopy = me;
+
+const meCopy2 = { ...me, kids: 2 };
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 8 */
+
+const personCard = ({ name, phone, eMail }) => `<div class="person">
+<h2>${name}</h2>
+<ul>
+    <li><a href="tel:+${phone}">Call</a></li>
+    <li><a href="mailto:${eMail}">E-mail</a></li>
+</ul>
+</div>`;
+
+const myPerson = {
+  name: "Clark Kent",
+  marriage: null,
+  glasses: true,
+  phone: 911,
+  street: "secret",
+  eMail: "clark@dailyplanet.com",
+};
+
+const { name, marriage, glasses, phone, street, eMail } = myPerson;
+
+console.log(personCard(myPerson));
+
+////////////////////////////////////////////////////////////////////////
+
+/* OPGAVE 9 */
+
+const getNums = () => [Math.random(), Math.random(), Math.random(), Math.random()];
+
+let randomNums = getNums();
+
+const [first, second, ...rest] = randomNums;
+
+console.log(randomNums);
+
+console.log("First number:", first, "Second number:", second);
