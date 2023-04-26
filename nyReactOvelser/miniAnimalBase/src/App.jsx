e10e28ff4c9314fa23d52dc2ace4f4f15a6f6745
fili5863​ <fili5863@stud.kea.dk>
2023-04-26 15:00:57 +0200

    animalBase done

--- nyReactOvelser/miniAnimalBase/src/App.jsx
+++ nyReactOvelser/miniAnimalBase/src/App.jsx
@@ -22,10 +22,35 @@ function cleanUpData() {
 function App() {
   const animals = cleanUpData();
   const [filter, setFilter] = useState("");
+  const [sortKey, setSortKey] = useState("");
+  const [sortDirection, setSortDirection] = useState("ASC");
 
-  let filterAnimals = [...animals];
+  let filteredAnimals = [...animals];
   if (filter) {
-    filterAnimals = filterAnimals.filter(ani => ani.type === filter);
+    filteredAnimals = filteredAnimals.filter(ani => ani.type === filter);
+  }
+
+  if (sortKey) {
+    filteredAnimals.sort((a, b) => {
+      if (a[sortKey] < b[sortKey]) {
+        return sortDirection === "ASC" ? -1 : 1;
+      }
+      if (a[sortKey] > b[sortKey]) {
+        return sortDirection === "ASC" ? 1 : -1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+  }
+  function setSorting(key) {
+    if (key === sortKey) {
+      //opposite
+      setSortDirection(old => (old === "ASC" ? "DESC" : "ASC"));
+    } else {
+      setSortDirection("ASC");
+    }
+    setSortKey(key);
   }
   return (
     <div className="App">
@@ -36,16 +61,24 @@ function App() {
       <table>
         <thead>
           <tr>
-            <td>Name</td>
-            <td>Trait</td>
-            <td>Type</td>
-            <td>Age</td>
+            <td>
+              <button onClick={() => setSorting("name")}>Name</button>
+            </td>
+            <td>
+              <button onClick={() => setSorting("trait")}>Trait</button>
+            </td>
+            <td>
+              <button onClick={() => setSorting("type")}>Type</button>
+            </td>
+            <td>
+              <button onClick={() => setSorting("age")}>Age</button>
+            </td>
           </tr>
         </thead>
         <tbody>
-          {filterAnimals.map(animal => (
+          {filteredAnimals.map(animal => (
             // eslint-disable-next-line react/jsx-key
-            <tr>
+            <tr key={animal.name}>
               <td>{animal.name}</td>
               <td>{animal.trait}</td>
               <td>{animal.type}</td>
